ef7148bb3f2d7cf09ded6482e8d12280a0943bf4
Lucas Salomão Boschiroli <152440853+lucasboschiroli@users.noreply.github.com>
2025-09-05 14:36:31 -0300

    Correções pontuais

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
         <a href="#" class="logo">CheckAudit</a>
         <nav>
             <a href="home.html" class="active">Home</a>
-            <a href="#">Bem - vindo(a) <?=$_SESSION['user_login']['username']?></a>
+            <a href="#"><?=$_SESSION['user_login']['username']?></a>
             <a href="../php/logout.php">Sair</a>
         </nav>
     </header>
