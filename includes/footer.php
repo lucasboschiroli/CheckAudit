a289942dde8bc82ff112ac0c85681f88b557e615
Lucas Salomão Boschiroli <152440853+lucasboschiroli@users.noreply.github.com>
2025-09-05 14:46:10 -0300

    Footer implementado

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+   <p>Copyright &copy; 2025 Equipe 01. Todos os direitos reservados.</p>
+</footer>
--- pages/home.php
+++ pages/home.php
@@ -13,6 +13,7 @@
 
 <body>
     <?php include "../includes/header.php";?> 
+    <?php include "../includes/footer.php";?> 
     
 </body>
 
